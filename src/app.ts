4a9c91da65e398e60ea2941055fa1296fb0f66ab
joaobdias <joaobdiasbr@gmail.com>
2025-08-16 12:41:25 -0300

    initializing api

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from "express"
+import "express-async-errors"
+
+import { routes } from "./routes"
+import { errorHandling } from "./middleware/error-handling"
 
 const PORT = 3333
 const app = express()
+
+app.use(routes)
+
 app.use(express.json())
+
+app.use(errorHandling)
 
 export { app, PORT }
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class UsersController {
+
+    create (request: Request, response: Response){
+
+    }
+
+}
+
+export { UsersController }
